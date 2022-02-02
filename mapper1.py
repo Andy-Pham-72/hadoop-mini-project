379d7a1a6a8a79dac5294a1cf9108cd0815d586a
Andy Pham <aqpham02@gmail.com>
2022-02-01 23:42:43 -0500

    update changes

--- mapper1.py
+++ mapper1.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
-<<<<<<< HEAD
 # mapper1 will map out vin_number and tuple of (incident type, make, and year).
-=======
-# Mapper Python Script - Will map out vin_number and tuple of ( ).
->>>>>>> 39de5cd20d0b594253e195f4af9ad4cf1887e88f
 
 import sys
 
@@ -13,10 +9,5 @@ for line in sys.stdin:
     line = line.strip()
     # Split by ","
     data = line.split(',')
-<<<<<<< HEAD
     # Print the combination of vin_number as key and value as incident type, make, and year
     print('%s\t(%s,%s,%s)'% (data[2],data[1],data[3],data[5]))
-=======
-    # Print/Map out values as combination of vin_number and tuple
-    print(f"{data[2]}\t{(data[1], data[3], data[5])}")
->>>>>>> 39de5cd20d0b594253e195f4af9ad4cf1887e88f
