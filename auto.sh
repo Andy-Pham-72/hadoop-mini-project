9fdaf50f539c5b0068423b42ac3f9b80471e3417
Andy Pham <70767722+Andy-Pham-72@users.noreply.github.com>
2022-02-03 15:46:34 -0500

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
+
 # Note we have to use the linux directory for python files which we uploaded in the Step 1
 -file /tmp/mapper1.py -mapper  "python mapper1.py" \
 -file /tmp/reducer1.py -reducer "python reducer1.py" \
+
+# Note for input and output used the HDFS directory
 -input /user/root/test_dir/data.csv -output /user/root/test_dir/output/all_accidents
 
 hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
