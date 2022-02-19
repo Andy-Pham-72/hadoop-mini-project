82e0dfcc9d64477c53ba94cf0cf4b97bf706b2ed
Andy Pham <aqpham02@gmail.com>
2022-02-18 20:05:30 -0500

    fixed directory

--- upload_files.sh
+++ upload_files.sh
@@ -1,6 +1,6 @@
-scp -P 2222 /hadoop-mini-project/data/data.csv root@127.0.0.1:/tmp/data.csv
-scp -P 2222 /hadoop-mini-project/mapper1.py root@127.0.0.1:/tmp/mapper1.py
-scp -P 2222 /hadoop-mini-project/mapper2.py root@127.0.0.1:/tmp/mapper2.py
-scp -P 2222 /hadoop-mini-project/reducer1.py root@127.0.0.1:/tmp/reduce1.py
-scp -P 2222 /hadoop-mini-project/reducer2.py root@127.0.0.1:/tmp/reduce2.py
-scp -P 2222 /hadoop-mini-project/auto.sh root@127.0.0.1:/tmp/auto.sh
+scp -P 2222 /hadoop-mini-project/data/data.csv root@127.0.0.1:/root
+scp -P 2222 /hadoop-mini-project/mapper1.py root@127.0.0.1:/root
+scp -P 2222 /hadoop-mini-project/mapper2.py root@127.0.0.1:/root
+scp -P 2222 /hadoop-mini-project/reducer1.py root@127.0.0.1:/root
+scp -P 2222 /hadoop-mini-project/reducer2.py root@127.0.0.1:/root
+scp -P 2222 /hadoop-mini-project/auto.sh root@127.0.0.1:/root
